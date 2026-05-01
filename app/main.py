6a65967bdb43c4a6a0ab1340fe2b53f9f7b3f470
Motlogelwa41-byte <boitshwarelomotlogelwa41@gmail.com>
2026-05-01 22:24:59 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from . import models, database
+
 models.Base.metadata.create_all(bind=database.engine)
+
 app = FastAPI(title="Ethical Edge Open GRC")
 
 def get_db():
