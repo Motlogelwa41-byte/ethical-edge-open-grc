c3537efca7bf91b86b5b9823c36265c73ad2229c
Motlogelwa41-byte <boitshwarelomotlogelwa41@gmail.com>
2026-05-01 22:38:42 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,22 +1,7 @@
-from fastapi import FastAPI, Depends
-from sqlalchemy.orm import Session
-from . import models, database
+from fastapi import FastAPI
 
-models.Base.metadata.create_all(bind=database.engine)
-
-app = FastAPI(title="Ethical Edge Open GRC")
-
-def get_db():
-    db = database.SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return {"status": "API Active"}
-
-@app.get("/frameworks/")
-def list_frameworks(db: Session = Depends(get_db)):
-    return db.query(models.Framework).all()
+def home():
+    return {"message": "Running"}
