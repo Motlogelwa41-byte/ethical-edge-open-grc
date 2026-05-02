57e45661fe517e98f311d9d4363752863889e56e
Motlogelwa41-byte <boitshwarelomotlogelwa41@gmail.com>
2026-05-02 19:32:30 +0200

    Update organizations.py

--- app/routers/organizations.py
+++ app/routers/organizations.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 
 from app.database.connection import SessionLocal
 from app.services import organization_service
+from app.schemas.organization import OrganizationCreate, OrganizationOut
 
 router = APIRouter(prefix="/organizations", tags=["Organizations"])
 
@@ -15,11 +16,18 @@ def get_db():
         db.close()
 
 
-@router.get("/")
+@router.get("/", response_model=list[OrganizationOut])
 def get_organizations(db: Session = Depends(get_db)):
     return organization_service.get_organizations(db)
 
 
-@router.post("/")
-def add_organization(name: str, industry: str, db: Session = Depends(get_db)):
-    return organization_service.create_organization(db, name, industry)
+@router.post("/", response_model=OrganizationOut)
+def add_organization(
+    org: OrganizationCreate,
+    db: Session = Depends(get_db)
+):
+    return organization_service.create_organization(
+        db,
+        org.name,
+        org.industry
+    )
