61f711dd4d806f08d25e38e044550b2207e01bc3
Motlogelwa41-byte <boitshwarelomotlogelwa41@gmail.com>
2026-05-03 10:58:06 +0200

    Update auth.py

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -2,8 +2,6 @@ from fastapi import APIRouter
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
-print("AUTH ROUTER FILE LOADED")
-
 @router.get("/ping")
 def ping():
-    return {"status": "auth is loading"}
+    return {"message": "Auth router working"}
