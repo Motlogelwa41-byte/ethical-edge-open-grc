60b8bedd41c34b79d9d59dc054870d9b75268f48
Motlogelwa41-byte <boitshwarelomotlogelwa41@gmail.com>
2026-05-05 20:03:43 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -25,22 +25,3 @@ def list_frameworks(db: Session = Depends(get_db)):
 def get_risks(db: Session = Depends(get_db)):
     return db.query(models.Risk).all()
 
-@app.post("/risks")
-def add_risk(
-    category: str,
-    description: str,
-    likelihood_score: int,
-    impact_score: int,
-    db: Session = Depends(get_db)
-):
-    risk = models.Risk(
-        category=category,
-        description=description,
-        likelihood_score=likelihood_score,
-        impact_score=impact_score
-    )
-
-    db.add(risk)
-    db.commit()
-    db.refresh(risk)
-    return risk
