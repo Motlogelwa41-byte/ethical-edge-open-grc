661565bdc403a9cb2378a287da0195e7c1af39e5
Motlogelwa41-byte <boitshwarelomotlogelwa41@gmail.com>
2026-06-10 23:20:00 +0200

    Update models.py

--- app/database/models.py
+++ app/database/models.py
@@ -1,8 +1,3 @@
 from sqlalchemy.ext.declarative import declarative_base
 
-# The single source of truth for your database models
 Base = declarative_base()
-
-# After defining Base, you can import your models here to register them
-# from .user import User
-# from .role import Role
