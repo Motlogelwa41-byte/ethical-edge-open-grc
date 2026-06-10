7e41498c6bbb5de188edf7815a7437b8a2967c6d
Motlogelwa41-byte <boitshwarelomotlogelwa41@gmail.com>
2026-06-10 23:21:07 +0200

    Update __init__.py

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,5 +1,2 @@
-from app.database.session import get_db, init_db, SessionLocal, engine
-from app.database.models import Base
-
-# Expose these symbols directly at the package level for your existing modules
-__all__ = ["get_db", "init_db", "SessionLocal", "engine", "Base"]
+from .models import Base
+from .session import get_db, init_db, SessionLocal, engine
