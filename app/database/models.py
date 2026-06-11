a74554a6aea1cff59eb631feac538638c5b00d05
Motlogelwa41-byte <boitshwarelomotlogelwa41@gmail.com>
2026-06-11 21:29:04 +0200

    Update models.py

--- app/database/models.py
+++ app/database/models.py
@@ -3,11 +3,10 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime
 
-# Initialize Base here, do NOT import it from this file
+# Initialize Base here
 Base = declarative_base()
 
 # Define your models below
 class ComplianceRecord(Base):
     __tablename__ = "compliance_records"
     id = Column(Integer, primary_key=True)
-    # ... rest of your model code
