7241101be48af0d20b7aefab80ce8e0355bca878
Motlogelwa41-byte <boitshwarelomotlogelwa41@gmail.com>
2026-06-14 15:48:23 +0200

    Update aws_observer.py

--- app/observers/aws_observer.py
+++ app/observers/aws_observer.py
@@ -1,5 +1,6 @@
 from sqlalchemy import text
 from app.observers.base_observer import BaseControlObserver
+
 class AWSObserver(BaseControlObserver):
     def __init__(self, region="us-east-1"):
         self.region = region
