7126192c913eaaf15bea43e0f6882ee6cf6b275c
Motlogelwa41-byte <boitshwarelomotlogelwa41@gmail.com>
2026-06-15 11:24:24 +0200

    Update orchestrator.py

--- orchestrator.py
+++ orchestrator.py
@@ -1,5 +1,7 @@
-import os
 import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from dotenv import load_dotenv
 
 # 1. Load environment variables from .env
