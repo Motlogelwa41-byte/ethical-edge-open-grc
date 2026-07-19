3dd4cc775f12163dface53f4a5d8b7be64bfc619
Motlogelwa41-byte <boitshwarelomotlogelwa41@gmail.com>
2026-07-19 18:02:14 +0200

    Update session.py

--- app/database/session.py
+++ app/database/session.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from app.database.models import Base
+from .models import Base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db" 
+# Using SQLite for integration testing as specified
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
