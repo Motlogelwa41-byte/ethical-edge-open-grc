eda6055e28a147a33e9a3af1a60e931ea55b8eda
Motlogelwa41-byte <boitshwarelomotlogelwa41@gmail.com>
2026-08-02 16:41:23 +0200

    Update models.py

--- app/auth/models.py
+++ app/auth/models.py
@@ -1,6 +1,6 @@
-<<<<<<< Updated upstream
 from sqlalchemy import Column, Integer, String, Boolean
 from app.database.models import Base
+
 
 class EnterpriseUser(Base):
     __tablename__ = "core_enterprise_users"
@@ -12,6 +12,3 @@ class EnterpriseUser(Base):
     can_access_king_v = Column(Boolean, default=True)
     can_access_professional_tier = Column(Boolean, default=False)
     can_access_enterprise = Column(Boolean, default=False)
-=======
-from sqlalchemy import Column, Integer, String, Boolean; from app.database.models import Base; class EnterpriseUser(Base): __tablename__ = "core_enterprise_users"; id = Column(Integer, primary_key=True, index=True); full_name = Column(String, nullable=True); email = Column(String, unique=True, index=True, nullable=False); can_access_king_v = Column(Boolean, default=True); can_access_professional_tier = Column(Boolean, default=False); can_access_enterprise = Column(Boolean, default=False)
->>>>>>> Stashed changes
